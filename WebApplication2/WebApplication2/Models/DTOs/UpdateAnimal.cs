58942a7c51c867b19e19dde828e0e431f9ceeef5
Rzelek <55500426+Rzelekk@users.noreply.github.com>
2024-04-19 20:23:33 +0200

    Add parameters at UpdateAnimal.cs

--- WebApplication2/WebApplication2/Models/DTOs/UpdateAnimal.cs
+++ WebApplication2/WebApplication2/Models/DTOs/UpdateAnimal.cs
@@ -1,6 +1,18 @@
-﻿namespace WebApplication2.Models.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+namespace WebApplication2.Models.DTOs;
 
 public class UpdateAnimal
 {
+    [MinLength(3)]
+    [MaxLength(200)]
+    public string Name { get; set; }
     
+    [MaxLength(200)]
+    public string Description { get; set; }
+    
+    [MaxLength(200)]
+    public string Category { get; set; }
+    
+    [MaxLength(200)]
+    public string Area { get; set; }
 }
